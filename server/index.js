5cbc369495879602d6034db853b4d898b8d52953
smaddi4 <88273838+smaddi4@users.noreply.github.com>
2022-11-18 07:23:27 +0530

    First piece of code committed

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const UserModel = require("./models/Users");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(
+  "mongodb+srv://vamsi444:Mongo%40444@cluster0.xx1wfae.mongodb.net/?retryWrites=true&w=majority"
+);
+
+app.get("/getUsers", (req, res) => {
+  UserModel.find({}, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+app.post("/createUser", async (req, res) => {
+  const user = req.body;
+  const newUser = new UserModel(user);
+  await newUser.save();
+  res.json(user);
+});
+
+app.listen(3001, () => {
+  console.log("Server is running");
+});
